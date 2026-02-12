1b0d354d4bea119f54e3b6d8c4e0b6f0a3207d54
Iker Figols <al435564@uji.es>
2026-02-12 11:39:25 +0100

    primera tabla?

---
+++ ei102725dfd/PAP_PATI.sql
@@ -0,0 +1,17 @@
+CREATE TABLE PAP_PATI (
+	dni		VARCHAR(9),
+	name		VARCHAR(50),
+	surname		VARCHAR(50),
+	password	VARCHAR(20),
+	email		VARCHAR(50),
+	phoneNumber	INTEGER,
+	address		VARCHAR(50),
+	tipo		VARCHAR(20),
+	available	BOOLEAN,
+	training	VARCHAR(50),
+	experience	VARCHAR(100),
+	gender		VARCHAR(15),
+	
+	CONSTRAINT cp_PAP_PATI PRIMARY KEY (dni),
+	CONSTRAINT calt_PAP_PATI UNIQUE (email),
+)
