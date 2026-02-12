ae2583d8a5a7c716720edea79ae7776d8e7c74df
Iker Figols <al435564@uji.es>
2026-02-12 14:59:07 +0100

    arregladas claves alternativas y Ids como INTEGERS, no he podido comprobar las tablas que se relacionan con otras :/

--- ei102725dfd/ACTIVITY.sql
+++ ei102725dfd/ACTIVITY.sql
@@ -1,15 +1,16 @@
 CREATE TABLE ACTIVITY(
-	idActivity		INTEGER,
-	title			VARCHAR(20),
-	tipo			VARCHAR(20),
+	idActivity		VARCHAR(10),
+	title			VARCHAR(100),
+	tipo			VARCHAR(30),
 	description		VARCHAR(100),
-	fecha			DATE,
+	date			DATE,
 	address			VARCHAR(20),
 	capacity		INTEGER,
 	sponsor			VARCHAR(20),
-	idInstructor		INTEGER
+	idInstructor		INTEGER,
 
-	CONSTRAINT cp_activity PRIMARY KEY(idActivity, title),
+	CONSTRAINT cp_activity PRIMARY KEY(idActivity),
+	CONSTRAINT calt_activity UNIQUE (title),
 	CONSTRAINT ca_activity FOREIGN KEY (idInstructor) REFERENCES INSTRUCTOR(idIns)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE
--- ei102725dfd/INSTRUCTOR.sql
+++ ei102725dfd/INSTRUCTOR.sql
@@ -3,8 +3,9 @@ CREATE TABLE INSTRUCTOR(
 	name		VARCHAR(20),
 	surname		VARCHAR(20),
 	email		VARCHAR(30),
-	phoneNumber	INTEGER(10),
+	phoneNumber	INTEGER,
 	expertise	VARCHAR(30),
 
-	CONSTRAINT cp_instrictor PRIMARY KEY (idIns, email)
+	CONSTRAINT cp_instrictor PRIMARY KEY (idIns),
+	CONSTRAINT calt_instructor UNIQUE (email)
 );
