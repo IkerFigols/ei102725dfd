44cc492cb670a0ac395a01041cfaa9eefbaae4da
al438643 <al438643@uji.es>
2026-02-15 14:24:18 +0100

    Create inserts.sql
    Inserts de candidacy hechos

---
+++ ei102725dfd/inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO Candidacy VALUES ('1','pending','2026-01-28','2','2'); 
+INSERT INTO Candidacy VALUES ('2','pending','2026-01-25','2','4'); 
+INSERT INTO Candidacy VALUES ('3','pending','2026-02-5','1','5');
+INSERT INTO Candidacy VALUES ('4','pending','2026-02-14','3','3');
+INSERT INTO Candidacy VALUES ('5','pending','2026-01-2','1','1');
