74528ab21dac0910ff697c7c5f6ba96423a136b5
al435443 <al435443@uji.es>
2026-02-16 22:02:41 +0100

    Inserts de negotiation

---
+++ ei102725dfd/negotiation_inserts.sql
@@ -0,0 +1,6 @@
+INSERT INTO Negotiation VALUES ('1', '2026-01-30', 'pending', '3', '4', '2');
+INSERT INTO Negotiation VALUES ('2', '2026-01-06', 'pending', '1', '4', '5');
+INSERT INTO Negotiation VALUES ('3', '2026-02-03', 'pending', '2', '4', '3');
+INSERT INTO Negotiation VALUES ('4', '2026-01-01', 'pending', '5', '4', '1');
+INSERT INTO Negotiation VALUES ('5', '2026-01-15', 'pending', '2', '2', '1');
+INSERT INTO Negotiation VALUES ('6', '2026-01-20', 'pending', '1', '2', '2');
