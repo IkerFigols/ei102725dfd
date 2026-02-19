38c6db8767e96c77e4df5c0045ab563085cb6ffc
al438643 <al438643@uji.es>
2026-02-19 10:43:22 +0100

    corregido por enesima vez
    si

--- ei102725dfd/candidacy.sql
+++ ei102725dfd/candidacy.sql
@@ -1,12 +1,17 @@
 CREATE TABLE Candidacy(
 
-idCandidacy	VARCHAR(10),
+idCandidacy	VARCHAR(9),
 status		VARCHAR(40) NOT NULL,
 dateAdded	DATE NOT NULL,
-idPapPati	VARCHAR(10) NOT NULL,
-idPapPatiList	VARCHAR(10) NOT NULL,
+idPapPati	VARCHAR(9) NOT NULL,
+idPapPatiList	VARCHAR(9) NOT NULL,
 
-CONSTRAINT  cp_candidacy PRIMARY KEY (idCandidacy),
+
+
+CONSTRAINT cp_candidacy PRIMARY KEY (idCandidacy),
+CONSTRAINT ck_len_idCan CHECK (LENGTH(idCandidacy) = 9),
+CONSTRAINT ck_len_idPapPati CHECK (LENGTH(idPapPati) = 9),
+CONSTRAINT ck_len_idPapPatiList CHECK (LENGTH(idPapPatiList) = 9),
 CONSTRAINT  ca_PapPati	 FOREIGN KEY (idPapPati) 
 	REFERENCES PapPati(dni)
 	ON DELETE  RESTRICT 
