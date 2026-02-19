7c7c48fa243b0a832b24e3469db4b4e8179bc962
Iker Figols <al435564@uji.es>
2026-02-19 12:19:26 +0100

    prueba

--- ei102725dfd/contract_inserts.sql
+++ ei102725dfd/contract_inserts.sql
@@ -1,8 +1,8 @@
 INSERT INTO Contract (idContract, startDate, endDate, document, salary, schedule, idNegotiation)
-VALUES ('CONTRATO01', '2024-01-01', '2024-12-31', '/docs/contrato_juan.pdf', 30000.00, 'L-V 09:00-18:00', NULL);
+VALUES ('CON000001', '2024-01-01', '2024-12-31', '/docs/contrato_juan.pdf', 30000.00, 'L-V 09:00-18:00', NULL);
 
 INSERT INTO Contract (idContract, startDate, endDate, document, salary, schedule, idNegotiation)
-VALUES ('CONTRATO02', '2024-02-15', '2025-02-15', '/docs/contrato_ana.docx', 55000.50, 'Turno Rotativo', 'NEGOCIA001');    --Suponiendo que existe la negoaciacion especificada
+VALUES ('CON000002', '2024-02-15', '2025-02-15', '/docs/contrato_ana.docx', 55000.50, 'Turno Rotativo', 'NEG000001');    --Suponiendo que existe la negoaciacion especificada
 
 INSERT INTO Contract (idContract, startDate, endDate, document, salary, schedule, idNegotiation)
 VALUES ('CONTRATO03', '2024-06-01', '2024-09-01', '/docs/temporal_luis.pdf', 1200.00, 'Fines de semana', NULL);
