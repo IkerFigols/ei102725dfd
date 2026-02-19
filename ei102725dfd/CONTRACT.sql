dedf04bc2e6855cb7445c17cc9bdec4ff268aa4f
Iker Figols <al435564@uji.es>
2026-02-19 13:26:39 +0100

    añadidos inserts y actualizada contract

--- ei102725dfd/CONTRACT.sql
+++ ei102725dfd/CONTRACT.sql
@@ -5,14 +5,8 @@ CREATE TABLE Contract (
 	document		VARCHAR(50)	NOT NULL,
 	salary			NUMERIC		NOT NULL,
 	schedule		VARCHAR(60)	NOT NULL, 	--VARCHAR?
-	idNegotiation		VARCHAR(9),
 
 	CONSTRAINT cp_contract PRIMARY KEY (idContract),
-	CONSTRAINT ca_contract FOREIGN KEY (idNegotiation) 
-		REFERENCES NEGOTIATION(idNegotiation)
-		ON DELETE SET NULL
-		ON UPDATE CASCAD,
-	CONSTRAINT id_longitud_10 CHECK (length(idContract) = 9),
-	CONSTRAINT salario_positivo CHECK (salary > 0),
-	CONSTRAINT idNegociacion_longitud_10 CHECK (idNegotiation IS NULL OR length(idNegotiation) = 10)
+	CONSTRAINT id_longitud_9 CHECK (length(idContract) = 9),
+	CONSTRAINT salario_positivo CHECK (salary > 0)
 );
