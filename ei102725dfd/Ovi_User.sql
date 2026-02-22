d07b8d9d55a949e686df2c0986ab98915219fdba
al438643 <al438643@uji.es>
2026-02-22 12:37:37 +0100

    OviUser corregido
    Tabla corregida
    Inserts conrregido
    Adjunto inserts de person

--- ei102725dfd/Ovi_User.sql
+++ ei102725dfd/Ovi_User.sql
@@ -1,20 +1,15 @@
 CREATE TABLE Ovi_User(
-dni		VARCHAR(10),
-name		VARCHAR(20) NOT NULL,
-surname		VARCHAR(30) NOT NULL,
-birthdayDate	DATE	    NOT NULL,
-password	VARCHAR(20) NOT NULL,
-email 		VARCHAR(30) NOT NULL,
-phoneNumber 	VARCHAR(9)  NOT NULL,
-address		VARCHAR(50) NOT NULL,
-legalGuardian 	VARCHAR(50),
-gender		VARCHAR(10) NOT NULL,
-
-CONSTRAINT  cp_oviUser PRIMARY KEY (dni),
-CONSTRAINT  cl_oviUser UNIQUE (email),
-CONSTRAINT  chk_phone CHECK ( LENGTH(phoneNumber) = 9 AND phoneNumber ~ '^[0-9]{9}$')
-CONSRAINT   chk_legalGuardian CHECK (
-                                        (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM birthdayDate) >= 18 AND legalGuardian IS NULL) OR
-                                        (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM birthdayDate) < 18 AND legalGuardian IS NOT NULL) 
-                                    )
+    dni             VARCHAR(10),
+    birthdayDate    DATE        NOT NULL,
+    password        VARCHAR(20) NOT NULL,
+    address         VARCHAR(50) NOT NULL,
+    legalGuardian   VARCHAR(50),
+    CONSTRAINT cp_oviUser PRIMARY KEY (dni),
+    CONSTRAINT ca_oviUser FOREIGN KEY (dni) REFERENCES PERSON(dni) 
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    CONSTRAINT chk_legalGuardian CHECK (
+        (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM birthdayDate) >= 18 AND legalGuardian IS NULL) OR
+        (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM birthdayDate) < 18 AND legalGuardian IS NOT NULL)
+    )
 );
