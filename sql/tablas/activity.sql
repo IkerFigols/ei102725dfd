7f9131ec80bc9259b91485614788b18cef47ff40
Iker Figols <al435564@uji.es>
2026-03-05 11:20:24 +0100

    tabla actividades añadida

---
+++ sql/tablas/activity.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Activity (
+	idActivity 		VARCHAR(9)	NOT NULL,
+	activityType		VARCHAR(20)	NOT NULL,
+	tittle			VARCHAR(50)	NOT NULL,
+	description		VARCHAR(300)	NOT NULL,
+	date 			DATE		NOT NULL,
+	address			VARCHAR(50)	NOT NULL,
+	capacity		NUMERIC,
+	sponsor			VARCHAR(20),
+	idInstructor		VARCHAR(9)	NOT NULL
+
+	CONSTRAINT cp_activity PRIMARY KEY (idActivity),
+	CONSTRAINT ca_instructor FOREIGN KEY (idInstructor) REFERENCES Instructor (dni)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE,
+	CONSTRAINT chk_id_activity CHECK (length(idActivity) = 9),
+	CONSTRAINT chk_id_instructor CHECK (length(idInstructor) = 9),
+	CONSTRAINT chk_capacity CHECK (capacity > 0)
+);
