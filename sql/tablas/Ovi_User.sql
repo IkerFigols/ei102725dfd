30548c70a867d10aa4ac26328a21998402263f3d
al438643 <al438643@uji.es>
2026-03-09 18:40:49 +0100

    Cambios hechos en ovi user y ap request
    hecho

--- sql/tablas/Ovi_User.sql
+++ sql/tablas/Ovi_User.sql
@@ -1,9 +1,13 @@
 CREATE TABLE Ovi_User(
-    dni             VARCHAR(9),
+    dni             VARCHAR(19),
     birthdayDate    DATE        NOT NULL,
     password        VARCHAR(20) NOT NULL,
     address         VARCHAR(50) NOT NULL,
     legalGuardian   VARCHAR(50),
+    accepted        BOOLEAN     NOT NULL,
+    reason          VARCHAR(250),
+    userPreferences VARCHAR(250) NOT NULL,
+
     CONSTRAINT cp_oviUser PRIMARY KEY (dni),
     CONSTRAINT ca_oviUser FOREIGN KEY (dni) REFERENCES PERSON(dni) 
         ON DELETE CASCADE 
@@ -12,5 +16,7 @@ CREATE TABLE Ovi_User(
         (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM birthdayDate) >= 18 AND legalGuardian IS NULL) OR
         (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM birthdayDate) < 18 AND legalGuardian IS NOT NULL)
     )
-    CONSTRAINT chk_dni CHECK(LENGTH(dni) = 9)
+    CONSTRAINT chk_dni CHECK(LENGTH(dni) = 9),
+    CONSTRAINT chk_reason CHECK( (reason IS NULL AND accepted IS TRUE)  OR
+    (reason IS NOT NULL AND accepted IS FALSE))
 );
--- sql/tablas/assistance_request.sql
+++ sql/tablas/assistance_request.sql
@@ -2,15 +2,18 @@ CREATE TABLE Assistance_Request(
 
 idAsReq		VARCHAR(9),
 data		DATE 	NOT NULL,
-tipo		VARCHAR(30) NOT NULL,
-userPreferences	VARCHAR(50) NOT NULL,
 idOviUser	VARCHAR(9) NOT NULL,
+state		VARCHAR(15)NOT NULL,
+reason 		VARCHAR(250),
 
 CONSTRAINT cp_assistance_request PRIMARY KEY (idAsReq),
 CONSTRAINT ca_idOviUser FOREIGN KEY (idOviUser) 
 	REFERENCES Ovi_User (dni)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE,
-CONSTRAINT chk_id CHECK(LENGTH(idAsReq) = 9)
-CONSTRAINT chk_ovi CHECK(LENGTH(idOviUser) = 9)
+CONSTRAINT chk_id CHECK(LENGTH(idAsReq) = 9),
+CONSTRAINT chk_ovi CHECK(LENGTH(idOviUser) = 9),
+CONSTRAINT chk_reason CHECK( (reason IS NULL AND state IN ('APROBADA', 'PENDIENTE')) OR
+(reason IS NOT NULL AND state = 'RECHAZADA'))
 );
+
