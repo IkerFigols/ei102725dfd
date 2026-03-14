ab5295fde0883af9ddbb9bf6dc1d978b1016408c
Iker Figols <al435564@uji.es>
2026-03-14 15:47:55 +0100

    añadidos los daos de papPati, contract, activity ademas de los modelos correspondientes

--- java/sgOvi/sgOvi/src/main/java/es/uji/ei1027/sgOvi/dao/ContractRowMapper.java
+++ java/sgOvi/sgOvi/src/main/java/es/uji/ei1027/sgOvi/dao/ContractRowMapper.java
@@ -1,26 +1,26 @@
 package es.uji.ei1027.sgOvi.dao;
 
+import es.uji.ei1027.sgOvi.model.Contract;
 import es.uji.ei1027.sgOvi.model.PapPati;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class PapPatiRowMapper implements RowMapper<PapPati> {
-    @Override
-    public PapPati mapRow(ResultSet rs, int rowNum) throws SQLException {
-        PapPati papPati = new PapPati();
-        papPati.setDni(rs.getString("dni"));
-        papPati.setPassword(rs.getString("password"));
-        papPati.setAddress(rs.getString("address"));
-        papPati.setType(rs.getString("type"));
-        papPati.setAvailable(rs.getBoolean("available"));
-        papPati.setTraining(rs.getString("training"));
-        papPati.setDocument(rs.getString("document"));
-        papPati.setReason(rs.getString("reason"));
-        papPati.setAccepted(rs.getBoolean("accepted"));
-        papPati.setPapPatiPreferences(rs.getString("papPatiPreferences"));
+public class
 
-        return papPati;
+ContractRowMapper implements RowMapper<Contract> {
+    @Override
+    public Contract mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Contract contract = new Contract();
+        contract.setIdContract(rs.getString("idContract"));
+        contract.setStartDate(rs.getDate("starDate"));
+        contract.setEndDate(rs.getDate("endDate"));
+        contract.setDocument(rs.getString("document"));
+        contract.setSalary(rs.getDouble("salary"));
+        contract.setSchedule(rs.getString("schedule"));
+        contract.setIdSelection(rs.getString("idSelection"));
+
+        return contract;
     }
 }
--- java/sgOvi/sgOvi/src/main/java/es/uji/ei1027/sgOvi/model/PapPati.java
+++ java/sgOvi/sgOvi/src/main/java/es/uji/ei1027/sgOvi/model/PapPati.java
@@ -1,4 +1,112 @@
 package es.uji.ei1027.sgOvi.model;
 
 public class PapPati {
+    private String dni;
+    private String password;
+    private String address;
+    private String type;
+    private boolean available;
+    private String training;
+    private String document;
+    private String reason;
+    private boolean accepted;
+    private String papPatiPreferences;
+
+    public PapPati() {}
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getTraining() {
+        return training;
+    }
+
+    public void setTraining(String training) {
+        this.training = training;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
+    }
+
+    public String getPapPatiPreferences() {
+        return papPatiPreferences;
+    }
+
+    public void setPapPatiPreferences(String papPatiPreferences) {
+        this.papPatiPreferences = papPatiPreferences;
+    }
+
+    @Override
+    public String toString() {
+        return "PapPati{" +
+                "dni='" + dni + '\'' +
+                ", password='" + password + '\'' +
+                ", address='" + address + '\'' +
+                ", type='" + type + '\'' +
+                ", available=" + available +
+                ", training='" + training + '\'' +
+                ", document='" + document + '\'' +
+                ", reason='" + reason + '\'' +
+                ", accepted=" + accepted +
+                ", papPatiPreferences='" + papPatiPreferences + '\'' +
+                '}';
+    }
 }
