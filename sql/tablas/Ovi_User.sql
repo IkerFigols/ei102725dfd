d139abf2482167c12731527b05ec1d3648a03591
al438643 <al438643@uji.es>
2026-04-08 11:57:22 +0200

    ovi cambiada tabla
    si

--- sql/tablas/Ovi_User.sql
+++ sql/tablas/Ovi_User.sql
@@ -4,7 +4,7 @@ CREATE TABLE Ovi_User(
     password        VARCHAR(20) NOT NULL,
     address         VARCHAR(50) NOT NULL,
     legalGuardian   VARCHAR(50),
-    accepted        BOOLEAN     NOT NULL,
+    accepted        VARCHAR(15) NOT NULL,
     reason          VARCHAR(250),
     userPreferences VARCHAR(250) NOT NULL,
 
@@ -12,11 +12,6 @@ CREATE TABLE Ovi_User(
     CONSTRAINT ca_oviUser FOREIGN KEY (dni) REFERENCES PERSON(dni) 
         ON DELETE CASCADE 
         ON UPDATE CASCADE,
-    CONSTRAINT chk_legalGuardian CHECK (
-        (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM birthdayDate) >= 18 AND legalGuardian IS NULL) OR
-        (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM birthdayDate) < 18 AND legalGuardian IS NOT NULL)
-    ),
     CONSTRAINT chk_dni CHECK(LENGTH(dni) = 9),
-    CONSTRAINT chk_reason CHECK( (reason IS NULL AND accepted IS TRUE)  OR
-    (reason IS NOT NULL AND accepted IS FALSE))
+    CONSTRAINT chk_reason CHECK( reason IN ('PENDING','ACCEPTED','REJECTED'))
 );
