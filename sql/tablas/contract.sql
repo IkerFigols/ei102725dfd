34fb33fb1f8c732eff95c2498e96ce52df791a97
Iker Figols <al435564@uji.es>
2026-04-08 11:59:29 +0200

    arreglado contract, espero

--- sql/tablas/contract.sql
+++ sql/tablas/contract.sql
@@ -5,14 +5,14 @@ CREATE TABLE Contract (
 	document		VARCHAR(50)	NOT NULL,
 	salary			NUMERIC		NOT NULL,
 	schedule		VARCHAR(60)	NOT NULL, 	--VARCHAR?
-	idSelection		VARCHAR(9),
+	idSelection		VARCHAR(9)	NOT NULL,
 
 	CONSTRAINT cp_contract PRIMARY KEY (idContract),
 	CONSTRAINT calt_contract UNIQUE (idSelection),
 	CONSTRAINT ca_contract FOREIGN KEY (idSelection) 
 		REFERENCES Selection(idSelection)
 		ON DELETE SET NULL
-		ON UPDATE CASCADE,
+		ON UPDATE RESTRICT,
 	CONSTRAINT id_longitud_10 CHECK (length(idContract) = 9),
 	CONSTRAINT salario_positivo CHECK (salary > 0),
 	CONSTRAINT idSelection_longitud_10 CHECK (idSelection IS NULL OR length(idSelection) = 9)
