09044053ed63769d2dd0e054150c9c5dbfbdaffb
Iker Figols <al435564@uji.es>
2026-05-02 19:20:56 +0200

    ME queda acabar los validadores

--- java/sgOvi/sgOvi/src/main/java/es/uji/ei1027/sgOvi/controller/TechnicianController.java
+++ java/sgOvi/sgOvi/src/main/java/es/uji/ei1027/sgOvi/controller/TechnicianController.java
@@ -45,7 +45,7 @@ public class TechnicianController {
     private PapPatiValidator papPatiValidator = new PapPatiValidator();
     private InstructorValidator instructorValidator = new InstructorValidator();
     private PersonDtoValidator personDtoValidator = new PersonDtoValidator();
-
+    private AssistanceRequestValidator assistanceRequestValidator = new AssistanceRequestValidator();
 
     @RequestMapping("/menuTechnician")
     public String menuTechnician(Model model) {
@@ -189,6 +189,10 @@ public class TechnicianController {
                                                         BindingResult bindingResult
     ) {
 
+        assistanceRequestValidator.validate(assistanceRequest, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "Technician/apManagement/update";
+        }
 
         if(assistanceRequest.getReason().isBlank())
             assistanceRequest.setReason(null);
